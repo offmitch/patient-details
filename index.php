0648dd516dea0a7cb98556123166a147744dd838
Mitchell MacDonald <mmacdonald157@my.bcit.ca>
2025-05-05 14:46:19 -0700

    initial commit index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>
+        <?php
+            echo "Hello, world! This is a simple PHP page.";
+        ?>
+    </p>
+</body>
+</html>
