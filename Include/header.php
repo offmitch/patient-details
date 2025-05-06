259271a028741d9fb61779ee601ccd4577fc8186
hugoamuan <hugo.amuan@icloud.com>
2025-05-06 16:40:23 -0700

    tiny mistake

--- Include/header.php
+++ Include/header.php
@@ -2,7 +2,7 @@
 <div class="header">
     <div class="inner_header">
         <div class="logo_container">
-            <img src=". /images/bcit_logo.jpg" alt="Site Logo" class="logo_image">
+            <img src="./images/bcit_logo.jpg" alt="Site Logo" class="logo_image">
             <h1><span>My</span><span>Site</span></h1>
         </div>
         <ul class="navigation">
