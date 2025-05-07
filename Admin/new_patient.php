c6c148519ba410a4ce3a096548b0e66adb0eb57a
Mitchell MacDonald <mmacdonald157@my.bcit.ca>
2025-05-06 20:03:34 -0700

    add some basics to new patient page

---
+++ Admin/new_patient.php
@@ -0,0 +1,29 @@
+<?php include("../Include/header.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patient Information System</title>
+    <link rel="stylesheet" href="/Style/styles.css">
+    <link rel="stylesheet" href="/Style/header.css">
+    <link rel="stylesheet" href="/Style/footer.css">
+    <link rel="stylesheet" href="/Style/patients.css">
+    <link rel="stylesheet" href="/Style/landing.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+<h1 style="padding: 100px">New Patient</h1>
+   
+
+<div class="results-container">
+   
+</div>
+
+
+</body>
+</html>
+
+
+<?php include("../Include/admin_footer.php"); ?>
