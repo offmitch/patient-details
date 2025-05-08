7e3734eed00cc4066475a948e611e1353d534beb
hugoamuan <hugo.amuan@icloud.com>
2025-05-08 15:17:45 -0700

    feat: minor changes

--- Include/header_auth.php
+++ Include/header_auth.php
@@ -1,8 +1,8 @@
 <div class="header">
 <div class="inner_header">
     <div class="logo_container">
-        <img src="../Images/bcit_logo.jpg" alt="Site Logo" class="logo_image">
-        <h1><span>My</span><span>Site</span></h1>
+        <img src="../images/bcit_logo.jpg" alt="Site Logo" class="logo_image">
+        <h1 style="padding-top: 25px"><span>My</span><span>Site</span></h1>
     </div>
     <div class="user_controls">
         <!-- TODO: Replace "John Doe" with dynamic name -->
