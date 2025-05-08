de3b3545f33722a1c9b2c46194d9fccc409abfd1
hugoamuan <hugo.amuan@icloud.com>
2025-05-08 15:09:19 -0700

    feat: student_patients page, plus header_auth

---
+++ Student/student_patients.php
@@ -0,0 +1,74 @@
+<?php include("../Include/header_auth.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Patient Information System</title>
+    <link rel="stylesheet" href="../Style/styles.css">
+    <link rel="stylesheet" href="../Style/header.css">
+    <link rel="stylesheet" href="../Style/footer.css">
+    <link rel="stylesheet" href="../Style/patients.css">
+    <link rel="stylesheet" href="../Style/landing.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+</head>
+<body>
+    <h1 style="padding: 20px">Patient List</h1>
+    
+    <div class="search-container">
+        <form method="GET" action="admin_patients.php">
+             <input type="text" name="first_name" placeholder="First Name"
+              value="<?php echo isset($_GET['first_name']) ? htmlspecialchars($_GET['first_name']) : ''; ?>">
+    <input type="text" name="last_name" placeholder="Last Name"
+      value="<?php echo isset($_GET['last_name']) ? htmlspecialchars($_GET['last_name']) : ''; ?>">
+    <input type="text" name="mrn" placeholder="MRN"
+      value="<?php echo isset($_GET['mrn']) ? htmlspecialchars($_GET['mrn']) : ''; ?>">
+
+      <input type="submit" value="Search" class="search-button">
+      <a href="admin_patients.php" class="all-button">All</a>
+  </form>
+</div>
+
+<div class="results-container">
+    <p>2 Results Found:</p>
+    <table class="results-table">
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>MRN</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>123456</td>
+                <td>
+                    <button class="view-button" onclick="window.location.href='admin_patient_details.php?mrn=789012'">View</button>
+                </td>
+            </tr>
+            <tr>
+                <td>Jane</td>
+                <td>Smith</td>
+                <td>789012</td>
+                <td>
+                    <button class="view-button" onclick="window.location.href='admin_patient_details.php?mrn=789012'">View</button>
+                    </button>
+                </td>
+
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<button class="add-button" onclick="window.location.href='new_patient.php'">Add New Patient</button>
+
+
+</body>
+</html>
+
+
+<?php include("../Include/admin_footer.php"); ?>
